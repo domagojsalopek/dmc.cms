4946f002c5241dd17d2e1327c0ad8c773c5e0d2b
domagojsalopek <domagoj.salopek@outlook.com>
2023-03-20 22:12:51 +0100

    Base Commit

---
+++ Dmc.Cms.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dmc.Cms.Web.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
